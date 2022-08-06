2f9cbb574afe609ea4da0fa772b3e5087d8805d8
Xavier Belda Tabet <xavier.belda@bestsecret.com>
2022-08-06 18:26:44 +0200

    Initial Commit

---
+++ ForecastiOSApp/ForecastiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastiOSAppApp.swift
+//  ForecastiOSApp
+//
+//  Created by Belda, Xavier on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ForecastiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
